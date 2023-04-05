5d409df87068159d4c004bfa42acaf240e0be481
TSERING WANGZES <2020csb1136@iitrpr.ac.in>
2023-04-05 12:07:28 +0530

    a

--- client/src/pages/FtyTable.js
+++ client/src/pages/FtyTable.js
@@ -49,7 +49,7 @@ function GlobalFilter({
 }
 
 function FtyTable({ columns, data }) {
-    var email = sessionStorage.getItem('email');
+    var email = localStorage.getItem('email');
     const navigate = useNavigate();
     // Use the state and functions returned from useTable to build your UI
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, // new
