2bb513a3b8039d0f2df27c31491d3b35d38df501
TSERING WANGZES <2020csb1136@iitrpr.ac.in>
2023-05-13 22:13:08 +0530

    newbend

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://department-database-blam.onrender.com"
+export const BACKEND_URL = "https://dep-2.onrender.com"
 
 // export const BACKEND_URL = "http://localhost:4002"
 
